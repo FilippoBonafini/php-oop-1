12fb00db7ff89d3282440b3b3b9ff3205f5b1987
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-04-14 17:52:21 +0200

    Creati commenti

--- Models/Movie.php
+++ Models/Movie.php
@@ -8,16 +8,21 @@ class Movie
         $this->genres = $genres;
     }
 
+    // FUNZIONE CHE RESTITUISCE TUTTE LE INFO DEL FILM 
     public function getInfo()
     {
         $info = "Title: {$this->title}, Genres: " . implode(', ', $this->genres);
         return $info;
     }
+
+    // FUNZIONE CHE RESTITUISCE IL TITOLO DEL FILM 
     public function getTitle()
     {
         $title = "Title: {$this->title}";
         return $title;
     }
+
+    // FUNZIONE CHE RESTITUISCE IL GENRE DEL FILM 
     public function getGeners()
     {
         $geners = "Genres: " . implode(', ', $this->genres);
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__ . '/Models/Movie.php';
 
+// CREIAMO NUOVI OGGETTI MOVIE 
 $movie1 = new Movie('Avatar', ['Fantasy', 'Azione']);
 $movie2 = new Movie('The ring', ['Horror']);
 
+// --------------------
+// STAMPIAMO I RISULTATI 
+// --------------------
 var_dump($movie1->getInfo());
 var_dump($movie2->getInfo());
 
